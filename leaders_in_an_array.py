10a179ba9889ce5c4bcde9b6e0239da7e0ab0941
Shree Dev <Shreedev2k3@gmail.com>
2023-07-20 13:05:23 +0530

    Add files via upload

---
+++ leaders_in_an_array.py
@@ -0,0 +1,13 @@
+def leaders(array):
+    lead=[]
+    maxi=-float('inf')
+    for i in range(len(array)-1,-1,-1):
+        if array[i]>maxi:
+            maxi=array[i]
+            lead.append(array[i])
+    lead.reverse()
+    return lead
+
+array=list(map(int,input().split()))
+print(leaders(array))
+    
